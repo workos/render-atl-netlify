8137a6d0008c5407fa0fba986faa95c4336384bd
Chance Strickland <hi@chance.dev>
2025-06-11 12:35:27 -0400

    add additional deps

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+// @ts-check
+import * as js from "@chance/eslint";
+import * as react from "@chance/eslint/react";
+import * as typescript from "@chance/eslint/typescript";
+
+export default [
+  //
+  js.config,
+  typescript.config,
+  react.config,
+];
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import netlifyPlugin from "@netlify/vite-plugin-react-router";
+import devtoolsJson from "vite-plugin-devtools-json";
 
 export default defineConfig({
   css: {
@@ -11,5 +12,5 @@ export default defineConfig({
       plugins: [tailwindcss, autoprefixer],
     },
   },
-  plugins: [reactRouter(), tsconfigPaths(), netlifyPlugin()],
+  plugins: [devtoolsJson(), reactRouter(), tsconfigPaths(), netlifyPlugin()],
 });
